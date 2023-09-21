5bc51bcf759cb1d5439d03cc7b45dfa4420296f7
Hafiz <komefumi@protonmail.ch>
2023-09-21 08:19:02 +0530

    Adds node to project

--- functions.php
+++ functions.php
@@ -15,3 +15,16 @@ function baw_hack_wp_title_for_home($title)
 }
 
 add_filter('wp_title', 'baw_hack_wp_title_for_home');
+
+function live_reload()
+{
+  $to_write = <<<WRITE
+  <script>document.write('<script src="http://'
+    + (location.host || 'localhost').split(':')[0]
+    + ':35729/livereload.js"></'
+    + 'script>')</script>
+  WRITE;
+  echo $to_write;
+}
+
+add_action('wp_footer', 'live_reload', 100);
